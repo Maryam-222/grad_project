11dedbc1b84d31d3039f895e842085836ca2ddfc
Abdelrahman Hossam <AbdelrahmanIsLearning@gmail.com>
2023-03-18 14:27:29 +0200

    Structure Changes

---
+++ FrontEnd/src/components/profile.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+
+function Userprofile() {
+    return (
+        <Container>
+            <h2 style={{ textAlign: 'center' }}>User Profile Page</h2>
+            <br />
+            <div className='userprofile'>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type='text' name='firstname' id='firstname' placeholder='Enter first name' />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type='text' name='lastname' id='lastname' placeholder='Enter last name' />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type='email' name='email' id='email' placeholder='Enter email' />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type='password' name='psw' id='psw' placeholder='Enter password' />
+                    </Form.Group>
+                    <Button variant='primary' type='submit'>
+                        Save
+                    </Button>
+                </Form>
+            </div>
+        </Container>
+    );
+}
+
+export default Userprofile;
